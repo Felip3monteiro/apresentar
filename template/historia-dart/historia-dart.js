b0bf4090913169f53b348aee3a51ae0eb22ad11b
Felip3ms <116771375+Felip3monteiro@users.noreply.github.com>
2024-10-09 00:22:01 -0300

    Add files via upload

---
+++ template/historia-dart/historia-dart.js
@@ -0,0 +1,41 @@
+$(document).ready(function() {
+    function activateCarousel() {
+        // Verifica se a tela é menor ou igual a 768px (mobile)
+        if ($(window).width() <= 768 && $('.navbar-mobile').length === 0) {
+            // Cria uma nova navbar móvel com carrossel apenas uma vez
+            $('body').prepend(`
+                <div class="navbar-mobile">
+                    <div class="nav-carousel">
+                        <a href="../template/historia-dart/historia-poo/historia-poo.html">Historia do POO</a>
+                        <a href="../template/atributos-dart/atr-dart.html">Atributos do Dart</a>
+                        <a href="../template/atr-paradigma/atr-para.html">Atributos do Paradigma</a>
+                    </div>
+                </div>
+            `);
+
+            // Ativa o carrossel na navbar mobile com setas nas laterais
+            $('.nav-carousel').slick({
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: false,
+                arrows: true,
+                prevArrow: "<button type='button' class='slick-prev'>&#9664;</button>",
+                nextArrow: "<button type='button' class='slick-next'>&#9654;</button>"
+            });
+        }
+    }
+
+    // Ativa o carrossel ao carregar a página, se necessário
+    activateCarousel();
+
+    // Verifica a largura da janela ao redimensionar
+    $(window).resize(function() {
+        // Remove a navbar mobile se a tela for maior que 768px (desktop)
+        if ($(window).width() > 768 && $('.navbar-mobile').length > 0) {
+            $('.navbar-mobile').remove();
+        } else {
+            activateCarousel(); // Reativa o carrossel se for redimensionado para uma tela menor
+        }
+    });
+});
